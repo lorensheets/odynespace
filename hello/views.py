6d45e453862ebd70caec58b9c2dc362edfa8437f
Loren Sheets <lorenmsheets@gmail.com>
2018-02-22 22:56:58 -0800

    changes

--- hello/views.py
+++ hello/views.py
@@ -16,7 +16,8 @@ def index(request):
 
         if form.is_valid():
             subject = "New Odyne Contact Form Submission"
-            message = form.cleaned_data['message']
+            message = 'Sender: ' + form.cleaned_data['first_name'] + ' ' + form.cleaned_data['last_name'] + \
+                '\nEmail: ' + form.cleaned_data['email'] + '\nMessage: ' + form.cleaned_data['message']
             sender = 'lorenmsheets@gmail.com'
             recipient = ['lorenmsheets@gmail.com']
             send_mail(subject, message, sender, recipient)
