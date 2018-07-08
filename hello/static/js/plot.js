ab42e68c26832ef547d4c6d38641e3d603e5db20
Loren Sheets <lorenmsheets@gmail.com>
2018-07-07 23:14:27 -0700

    changes

--- hello/static/js/plot.js
+++ hello/static/js/plot.js
@@ -1,6 +1,19 @@
+var data;
+
+// Parse local CSV file
+Papa.parse('flightRecorder.csv', {
+	complete: function(results) {
+    data = results;
+		console.log("Finished:", results.data);
+	}
+});
+
+
+
 TESTER = document.getElementById('tester');
 
 Plotly.plot( TESTER, [{
     x: [1, 2, 3, 4, 5],
     y: [1, 2, 4, 8, 16] }], {
-    margin: { t: 0 } } );
+      margin: { t: 0 }
+    } );
